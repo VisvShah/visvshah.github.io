b85af74a3d4e499b56e0c085471b66c55d903eb4
VisvShah <visvshah1@gmail.com>
2022-06-29 13:19:19 -0400

    Navigation Bar Part 2
    -Finished Left Side of topbar
    -started menu (right side drop-down)

--- src/Components/Topbar/Topbar.jsx
+++ src/Components/Topbar/Topbar.jsx
@@ -4,15 +4,19 @@ import gitLogo from '../../assets/githubLogo.png'
 import linkedinLogo from '../../assets/linkedinLogo.png'
 export default function TopBar() {
   return (
-    <div className = 'topbar'>
+    <div className = 'topbar active'>
         <div className = 'header'>
             <div className="leftSide">
                 <a href = '#about' className = 'logo'>visvshah.com</a>
                 <a href = 'https://github.com/VisvShah' target='_blank' rel="noreferrer noopener"> <img src = {gitLogo} alt = 'Github Logo' className = 'gitLogo'></img></a>
-                <a href = 'www.linkedin.com/in/visvshah' target='_blank' rel="noreferrer noopener"> <img src = {linkedinLogo} alt = 'Linkedin Logo' className = 'linkedinLogo'></img></a>
+                <a href = 'https://www.linkedin.com/in/visvshah/' target='_blank' rel="noreferrer noopener"> <img src = {linkedinLogo} alt = 'Linkedin Logo' className = 'linkedinLogo'></img></a>
             </div>
             <div className="rightSide">
-                <a href = '#about'>INTRO</a>
+                <div className="menu">
+                    <span className = "line"></span>
+                    <span className = "line"></span>
+                    <span className = "line"></span>
+                </div>
             </div>
         </div>
     </div>
