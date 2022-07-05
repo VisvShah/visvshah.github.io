3e145a4d9a8c67f82b2efc56a15cf0edbaf498f7
VisvShah <visvshah1@gmail.com>
2022-07-05 01:30:50 -0400

    Projects Page Part 1
    -brushed up on about page
    -Created Structure for page
    -designed projectData const to manage varying data between projects

--- src/Components/Contact/Contacts.jsx
+++ src/Components/Contact/Contacts.jsx
@@ -1,9 +1,12 @@
 import React from 'react'
 import './contacts.scss'
+import LinkedInIcon from '@mui/icons-material/LinkedIn';
+import ArrowCircleRightIcon from '@mui/icons-material/ArrowCircleRight';
 export default function Contacts() {
   return (
     <div className = 'contacts' id = 'contacts'>
-        
+        <LinkedInIcon className = 'test'/>
+        <ArrowCircleRightIcon className='test'/>
     </div>
   )
 }
--- src/Components/Projects/Projects.jsx
+++ src/Components/Projects/Projects.jsx
@@ -1,9 +1,68 @@
-import React from 'react'
-import './projects.scss'
+import React from 'react';
+import './projects.scss';
+import ArrowCircleLeftIcon from '@mui/icons-material/ArrowCircleLeft';
+import ArrowCircleRightIcon from '@mui/icons-material/ArrowCircleRight';
 export default function Projects() {
+      const projectData = [
+        {
+            id: "0",
+            title: "Dragon Dragon Dash - A Mobile Game",
+            link: "https://apps.apple.com/tt/app/dragon-dragon-dash/id1621746085",
+            videoLink: "https://youtu.be/Kq7K9lWkZtI",
+            skills: "Swift, Spritekit",
+            date: "April 2022 - May 2022",
+            desc: "This is a game I developed to achive my goal of publishing an app on the app store. This casual mobile game includes: a variety of levels with altering difficulty, 7 different enemy types, 4 impactful upgrade systems, 4 unique power-ups, and fun controls. This was a fun project to make and it is made up of over 1500 lines of code. I completed it with image and audio assetts and it is now as functional as any other popular game on the app store.",
+        },
+        
+        {
+            id: "1",
+            title: "This Personal Portfolio",
+            link: "https://apps.apple.com/tt/app/dragon-dragon-dash/id1621746085",
+            videoLink: "https://youtu.be/Kq7K9lWkZtI",
+            skills: "JavaScript, React.js",
+            date: "June 2022",
+            desc: "This personal portfolio furthered my expereince with several asppects of React.js.",
+        },
+        {
+            id: "2",
+            title: "RankR - A Full Stack Social Media Site",
+            link: "https://apps.apple.com/tt/app/dragon-dragon-dash/id1621746085",
+            videoLink: "https://youtu.be/Kq7K9lWkZtI",
+            skills: "JavaScript, React.js, Node.js, Express.js, MongoDB.js",
+            date: "July 2022 - August 2022",
+            desc: "in construction...",
+        },
+        {
+            id: "3",
+            title: "RankR - A Full Stack Social Media Site",
+            link: "https://apps.apple.com/tt/app/dragon-dragon-dash/id1621746085",
+            videoLink: "https://youtu.be/Kq7K9lWkZtI",
+            skills: "JavaScript, React.js, Node.js, Express.js, MongoDB.js",
+            date: "July 2022 - August 2022",
+            desc: "in construction...",
+        },
+    ];
   return (
     <div className = 'projects' id = 'projects'>
-
+        <div className="projectSlider">
+            {projectData.map((pD) => (
+            <div className="projectHolder">
+                <div className="project">
+                    <div className="top">
+                        <h1>{pD.title}</h1>
+                    </div>
+                    <div className="bottom">
+                        <div className="left">
+                        </div>
+                        <div className="right">
+                        </div>
+                    </div>
+                </div>
+            </div>
+            ))}
+        </div>
+        <ArrowCircleLeftIcon className='leftArrow'/>
+        <ArrowCircleRightIcon className = 'rightArrow'/>
     </div>
   )
 }
